7ed5889bca50c9995fa0934dd1bb76c153d8fdaf
Phil Smith <admin@nudgeai.uk>
2025-08-15 14:43:40 +0100

    [ADD] Add Marquee component for scrolling logos

---
+++ src/components/ui/marquee.tsx
@@ -0,0 +1,53 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+interface MarqueeProps extends React.HTMLAttributes<HTMLDivElement> {
+  reverse?: boolean;
+  pauseOnHover?: boolean;
+  vertical?: boolean;
+  repeat?: number;
+  [key: string]: any;
+}
+
+export function Marquee({
+  className,
+  reverse,
+  pauseOnHover = false,
+  vertical = false,
+  children,
+  repeat = 4,
+  ...props
+}: MarqueeProps) {
+  return (
+    <div
+      {...props}
+      className={cn(
+        "group flex overflow-hidden p-2",
+        {
+          "flex-row": !vertical,
+          "flex-col": vertical,
+        },
+        className
+      )}
+    >
+      {Array(repeat)
+        .fill(0)
+        .map((_, i) => (
+          <div
+            key={i}
+            className={cn("flex shrink-0 justify-around", {
+              "animate-marquee flex-row": !vertical,
+              "animate-marquee-vertical flex-col": vertical,
+              "group-hover:[animation-play-state:paused]": pauseOnHover,
+              "[animation-direction:reverse]": reverse,
+            })}
+            style={{
+              "--gap": "1rem",
+            } as React.CSSProperties}
+          >
+            {children}
+          </div>
+        ))}
+    </div>
+  );
+}
