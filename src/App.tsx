dcccca908f6c5b17cbad1d61a55f511472096dcb
Phil Smith <admin@nudgeai.uk>
2025-08-29 00:42:57 +0100

    Update App.tsx
    remove analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,8 @@ import Features from './components/Features'
 import EmailCapture from './components/EmailCapture'
 import Footer from './components/Footer'
 import { Toaster } from 'sonner'
-import { useEffect } from 'react'
-import { initAnalytics, trackPageView } from '@/lib/analytics'
 
 function App() {
-  useEffect(() => {
-    initAnalytics()
-    trackPageView()
-  }, [])
-
   return (
     <div className="min-h-screen bg-black text-white">
       <Toaster position="top-right" theme="dark" />
