20ef6053e076eaad70e37f21d006f7519afe8d12
Phil Smith <admin@nudgeai.uk>
2025-08-29 13:21:26 +0100

    [FEAT] Create HomePage component for main landing page

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,15 @@
+import Hero from './Hero'
+import Features from './Features'
+import EmailCapture from './EmailCapture'
+import Footer from './Footer'
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <EmailCapture />
+      <Footer />
+    </>
+  )
+}
