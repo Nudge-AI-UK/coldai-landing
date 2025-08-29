07a94659fc95dc32b71c8e1dfd4dc764525da4c4
Phil Smith <admin@nudgeai.uk>
2025-08-29 13:38:17 +0100

    [ENHANCE] Add global background animation to App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,31 @@ import Blog from './components/Blog'
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-black text-white">
-        <Toaster position="top-right" theme="dark" />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/blog" element={<Blog />} />
-        </Routes>
+      <div className="min-h-screen bg-gray-900 text-white relative">
+        {/* Global animated background - matching Hero style */}
+        <div className="fixed inset-0 overflow-hidden pointer-events-none">
+          <div 
+            className="absolute top-0 -left-1/4 w-1/2 h-1/2 bg-orange-500/10 rounded-full blur-3xl animate-pulse"
+            style={{ animationDuration: '6s' }}
+          />
+          <div 
+            className="absolute bottom-0 -right-1/4 w-1/2 h-1/2 bg-blue-500/10 rounded-full blur-3xl animate-pulse"
+            style={{ animationDuration: '8s', animationDelay: '2s' }}
+          />
+          <div 
+            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/3 h-1/3 bg-purple-500/10 rounded-full blur-3xl animate-pulse"
+            style={{ animationDuration: '7s', animationDelay: '1s' }}
+          />
+        </div>
+        
+        {/* Main content */}
+        <div className="relative z-10">
+          <Toaster position="top-right" theme="dark" />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/blog" element={<Blog />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   )
